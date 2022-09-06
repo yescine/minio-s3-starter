f9aa7e1fadabb3da6a7705c284c9be2b240ffdb7
yescine <bouchoucha.yassine@gmail.com>
2022-09-06 09:22:08 +0100

    multiple file upload with folder path

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -40,6 +40,31 @@ router.post("/file", fUpload.single("file"), async (req, res, next) => {
   res.send({ message });
 });
 
+router.post("/files", fUpload.array("files",5), async (req, res, next) => {
+  // console.log({ body: req.body, file: req.files });
+  const files = req.files
+  // @ts-ignore
+  files.forEach(async(file,idx)=>{
+    const metaData = {
+      "Content-Type": "application/octet-stream",
+      "X-Amz-Meta-Testing": 1234,
+      example: 5678,
+      project: "e-erp-test",
+    };
+    const result: UploadedObjectInfo = await new Promise((resolve, reject) => {
+      minioClient.fPutObject("test-dev", `avatar/${file.filename}`, file.path, metaData, (err, result) => {
+        if (err) console.log(err);
+        resolve(result);
+      });
+    });
+    const message = `file ${file.filename} uploaded with etag = ${result.etag}`;
+    console.log(message);
+  })
+  // @ts-ignore
+  const message = `uploaded: ${files.map(file=>file.filename).join('-')}`
+  res.send({ message });
+});
+
 router.get("/file_test", (req, res, next) => {
   const fileName = (req.query.fileName || "pdf-document") as string;
   const objTestPath = "C:/Users/bouch/Documents/cooltool-benchmark-12-08-2022.pdf";
