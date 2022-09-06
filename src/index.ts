76c18b17d23d703a8eee7f9632e2021f9c171fed
yescine <bouchoucha.yassine@gmail.com>
2022-09-06 05:57:23 +0100

    implement simple file upload

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import  express from 'express'
+import {Client as MinioClient} from 'minio'
+import dotenv from 'dotenv'
 import allRoute from './routes'
+
+dotenv.config();
+
 const app = express()
+
+export const minioClient = new MinioClient({
+  endPoint: 'localhost',
+  port: 9000,
+  useSSL: false,
+  accessKey: process.env.MINIO_USER,
+  secretKey: process.env.MINIO_PASSWORD
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -10,5 +23,6 @@ app.use('/api',...allRoute)
 
 const PORT = 5000
 app.listen(PORT,()=>{
+  // console.log({env:process.env.MINIO_USER})  
   console.log(`minio client listening on ${PORT}`)
 })
--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,8 +1,20 @@
 import {Router} from 'express'
-
+import {minioClient} from '../../src/index'
 const router =  Router()
 
 router.get('/file',(req,res,next)=>{
+  const fileName = (req.query.fileName || 'pdf-document' )as string
+  const objTestPath = "C:/Users/bouch/Documents/cooltool-benchmark-12-08-2022.pdf"
+  const metaData = {
+    'Content-Type': 'application/octet-stream',
+    'X-Amz-Meta-Testing': 1234,
+    'example': 5678,
+    'project':'e-erp'
+}
+  minioClient.fPutObject('test-dev',fileName,objTestPath,metaData,(err,result)=>{
+    if(err) console.log(err)
+    console.log(`${fileName} uploaded successfully`)
+  })
   res.send('sent file')
 })
 
