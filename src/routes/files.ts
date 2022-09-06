4b998194d89e7da2823a3dbad643a7381261e83d
yescine <bouchoucha.yassine@gmail.com>
2022-09-06 12:52:15 +0100

    get object from minio client

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -3,7 +3,7 @@ import { minioClient } from "../../src/index";
 import multer from "multer";
 import { UploadedObjectInfo } from "minio";
 const router = Router();
-
+const bucket = "test-dev"
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./upload");
@@ -30,7 +30,7 @@ router.post("/file", fUpload.single("file"), async (req, res, next) => {
     project: "e-erp-test",
   };
   const result: UploadedObjectInfo = await new Promise((resolve, reject) => {
-    minioClient.fPutObject("test-dev", filename, path, metaData, (err, result) => {
+    minioClient.fPutObject(bucket, filename, path, metaData, (err, result) => {
       if (err) console.log(err);
       resolve(result);
     });
@@ -52,7 +52,7 @@ router.post("/files", fUpload.array("files",5), async (req, res, next) => {
       project: "e-erp-test",
     };
     const result: UploadedObjectInfo = await new Promise((resolve, reject) => {
-      minioClient.fPutObject("test-dev", `avatar/${file.filename}`, file.path, metaData, (err, result) => {
+      minioClient.fPutObject(bucket, `avatar/${file.filename}`, file.path, metaData, (err, result) => {
         if (err) console.log(err);
         resolve(result);
       });
@@ -63,6 +63,19 @@ router.post("/files", fUpload.array("files",5), async (req, res, next) => {
   // @ts-ignore
   const message = `uploaded: ${files.map(file=>file.filename).join('-')}`
   res.send({ message });
+});
+
+router.get("/object", (req, res, next) => {
+  const {fileName,etag} = req.query
+  res.render('minio/object')
+  minioClient.statObject(bucket,fileName.toString(),(err,stat)=>{
+    if(err) console.log(err)
+    console.log({stat})
+  })
+  minioClient.presignedGetObject(bucket,fileName.toString(),(err,url)=>{
+    if(err) console.log(err)
+    console.log({url})
+  })
 });
 
 router.get("/file_test", (req, res, next) => {
